35e0951168900fbf754571d0b06ba3d07bfb7026
bheema-284 <bheemudug@gmail.com>
2022-06-23 19:06:29 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import WeatherData from './components/WeatherData';
+function App() {
+  return (
+    <div className="App">
+      <WeatherData />
+    </div>
+  );
+}
+
+export default App;
