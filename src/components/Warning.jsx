6577364e9a51bb52ecd625fa626de0dff582aca8
bheema-284 <bheemudug@gmail.com>
2022-06-24 21:08:33 +0530

    Some changes

--- src/components/Warning.jsx
+++ src/components/Warning.jsx
@@ -7,8 +7,8 @@ function Warning() {
     <div className="Warning">
       <h2> No location found </h2>
       <p>Try informing city/town and state/country</p>
-      <p> Ex: São Carlos, São Paulo</p>
-      <p> Ex: Tokyo, Japan </p>
+      <p> Ex: Metro City</p>
+      <p> Ex: Hyderabad, Mumbai </p>
     </div>
   );
 }
