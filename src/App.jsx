c54ec17233d23b97efaa872bba8a5f8d449d224e
bheema-284 <bheemudug@gmail.com>
2022-07-19 17:47:04 +0530

    some UI updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 import { Home } from "./components/Home";
 
-function App() {
-  const [isLoding, setIsLoding] = useState(true);
-  setTimeout(() => {
-    setIsLoding(false);
-  }, 3000);
+function App() {  
   return (
-    <div className="main">
-      {isLoding ? (
-        <div className="loading">
-          <div className="main">
-            <div className="left"></div>
-            <div className="right"></div>
-          </div>
-        </div>
-      ) : (
-        ""
-      )}
+    <div >      
       <Home />
     </div>
   );
